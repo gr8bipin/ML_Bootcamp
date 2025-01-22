98f1a0ca12b403e366966a4246f3d76522177241
Bipin Shrestha <bipinhyoju@gmail.com>
2025-01-22 19:39:13 +0545

    Python AI/ML Bootcamp

---
+++ day1.py
@@ -0,0 +1,44 @@
+def test():
+    bool()
+
+print(test)
+
+print(test())
+
+print(bool(test))
+
+print(bool(test()))
+
+print(bool(None))
+
+print(bool())
+
+print(bool(0))
+
+print(bool(False))
+
+print(bool(""))
+
+print(bool(1))
+
+print(bool(1.0))
+
+print(bool("1"))
+
+print(bool(2))
+
+# Floor division
+print(10 // 3)
+
+# Modulo division
+print(10 % 3)
+
+# string concatenation
+print("Hello, " + "World!")
+
+# separator
+print("Hello", "World", "!")
+
+# format string
+name = "John"
+print(f"Hello, {name}!")
